2b20481834628c1e7985ca7c12ed0f6744224476
David Veller (Partner) <QNM367@motorolasolutions.com>
2024-08-19 12:53:25 -0400

    we got da data

--- get_trains.py
+++ get_trains.py
@@ -1,11 +1,12 @@
-from google.transit import gtfs_realtime_pb2
+import nyct_subway_pb2
 import requests
 
-feed = gtfs_realtime_pb2.FeedMessage()
-response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace')
+feed = nyct_subway_pb2.gtfs__realtime__pb2.FeedMessage()
+response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l')
 feed.ParseFromString(response.content)
 f = open("train_info.txt", "w")
 for entity in feed.entity:
   f.write(str(entity))
+  print(entity.id)
 
 f.close()
