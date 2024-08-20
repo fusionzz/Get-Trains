3a5b9550de27626009f2889ace4a0bb41ad61471
David <davidveller@gmail.com>
2024-08-20 13:18:44 -0400

    trying to get all fields (specifically nyct field)

--- get_trains.py
+++ get_trains.py
@@ -1,12 +1,12 @@
 import nyct_subway_pb2
 import requests
+from google.protobuf.json_format import MessageToDict
 
 feed = nyct_subway_pb2.gtfs__realtime__pb2.FeedMessage()
 response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-l')
 feed.ParseFromString(response.content)
 f = open("train_info.txt", "w")
-for entity in feed.entity:
-  f.write(str(entity))
-  print(entity.id)
-
+f.write(str(feed))
+#print(MessageToDict(feed.entity))
+print(vars(feed.entity))
 f.close()
